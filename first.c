e05dad79a3258c875242b147e7ce3abcb5b2c989
Gelilamihirke <mihirkelala@gmail.com>
2023-10-17 09:58:35 -0700

    try

---
+++ first.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * _printf - prints the parameters given to it
+ * @format: format of character given to iit
+ * Return: number of element
+ */
+int _printf(const char *format, ...)
+{
+	va_list arr;
+	int i = 0, count = 0;
+
+	if (!format)
+		return (-1);
+	else if (*format == '%')
+		return (-1);
+	va_start(arr, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			if (format[i = 1] == '\0')
+				return (-1);
+			else if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				count++;
+				i++;
+			}
+			else if (cmp_func(format[i + 1]) != NULL)
+			{
+				count += (cmp_func(format[i + 1]))(arr);
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(arr);
+	return (count);
+}
--- main.h
+++ main.h
@@ -4,5 +4,29 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
 int _putchar(char c);
+int _printf(const char *format, ...);
+int print_num(va_list n);
+int print_string(va_list s);
+int _strlen(const char *s);
+int (*cmp_func(const char a))(va_list);
+int print_char(va_list arr);
+void print_bin(unsigned int num);
+int print_bin(va_list arr);
+int print_unsigned(va_list n);
+/**
+ * struct types - specifier structure for printf
+ * @p: pointer
+ * @func: function
+ */
+typedef struct types
+{
+	char p;
+	int (*func)(va_list);
+}
+myprintf;
+
 #endif
